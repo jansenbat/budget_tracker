2bcc45a10f92958adf8954d9993127f33d5d2aa4
Jansen Rodrigo <103814467+jansenbat@users.noreply.github.com>
2022-08-27 14:30:23 -1000

    Sandbox for new functions

---
+++ budgettrack_sandbox.py
@@ -0,0 +1,32 @@
+"""
+Where I create and play with functions before adding it to the main budget_tracker.py file
+"""
+
+expense1 = {
+    'water' : 250,
+    'electric' : 150,
+    'internet' : 150,
+    'house insurance' : 400
+}
+expense2 = {
+    'maintenance' : 300,
+    'dog' : 250
+}
+expense_total = sum(expense1.values()) + sum(expense2.values())
+
+income1 = {
+    'salary' : 2500,
+    'invest' : 1000
+}
+income2 = {
+    'dog' : 1000
+}
+income_total = sum(income1.values()) + sum(income2.values())
+
+budget_plan = {
+    'expenses' : 50,
+    'savings' : 20,
+    'wants' : 30
+}
+
+# write sandbox functions here
