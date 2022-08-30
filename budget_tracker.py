473717702039383002a0df6ea0294493f9450b41
Jansen Rodrigo <103814467+jansenbat@users.noreply.github.com>
2022-08-30 00:42:50 -1000

    "Completed" budget tracker
    Completed is in quotation marks because I might add new features in the future, but right now I'm happy with it.

--- budget_tracker.py
+++ budget_tracker.py
@@ -33,11 +33,13 @@ budget_plan = {
     'wants' : 30,
     'savings' : 20
 }
+
 budget_info = {
     'exp_plan' : int(inc_total * (budget_plan['expenses'] / 100)),
     'want_plan' : int(inc_total * (budget_plan['wants'] / 100)),
     'save_plan' : int(inc_total * (budget_plan['savings'] / 100))
 }
+
 
 # this function adds new entries to specific dictionaries
 def add_to(aDict):
@@ -45,9 +47,26 @@ def add_to(aDict):
     aDict: any dictionary to add to
     name_of: a string, just used to print the name of the dictionary being interacted with
     """
-    add_name = input("What would you like to enter: ")
-    add_amount = input("What is the amount of the item: ")
-    aDict[add_name.lower()] = int(add_amount)
+    print(aDict)
+    while True:
+        add_name = str(input("What would you like to enter (q to quit): ")).lower()
+        if add_name == 'q':
+            break
+        else:
+            add_amount = int(input("What is the amount of the item: "))
+            print(add_name + " : " + str(add_amount))
+            add_confirm = input("Do you wish to confirm this entry? y/n: ").lower()
+            if add_confirm == 'y':
+                aDict[add_name.lower()] = int(add_amount)
+                print(aDict)
+                print("Item successfully added.")
+            elif add_confirm == 'n':
+                print("Item will not be added.")
+                continue
+            else:
+                print("Invalid input, try again.")
+                continue
+
 
 # this function deletes a key-value pair from a dictionary
 def del_from(aDict):
@@ -57,7 +76,7 @@ def del_from(aDict):
     """
     while True:
         print(aDict)
-        del_name = input("Which item would you like to delete ('q' to quit)? Enter name: ").lower()
+        del_name = input("Which item would you like to delete (q to quit)? Enter name: ").lower()
         if del_name == 'q':
             break
         elif del_name in aDict:
@@ -89,6 +108,7 @@ def del_from(aDict):
                 print("Invalid input.")
                 continue   # send the user back to the top of the while loop
 
+
 # this function modifies the value of an already existing item in a dictionary
 def mod_amount(aDict):
     """
@@ -96,7 +116,7 @@ def mod_amount(aDict):
     """
     while True:
         print(aDict)
-        to_mod = input("Which item's value would you like to change ('q' to quit)? ")
+        to_mod = input("Which item's value would you like to change (q to quit)? ")
         if to_mod == 'q':
             print("Going back to the main page.")
             break
@@ -120,15 +140,6 @@ def mod_amount(aDict):
             print("Invalid input. Try again.")
             continue
 
-# this function allows the user to view specific dictionaries
-def view_dict(aDict, name_of):
-    """
-    aDict: any dictionary the user would like to view
-    name_of: just used to display the name of the dictionary being accessed
-    """
-    print("Currently Viewing: " + name_of)
-    for key, value in aDict.items():
-        print(key + " : " + str(value))
 
 # this function allows the user to change their budget plan
 def change_plan(bp):
@@ -141,6 +152,7 @@ def change_plan(bp):
         new_wants = int(input("How much would you like to allocate towards wants? "))
         new_savings = int(input("How much would you like to allocate towards savings? "))
         new_total = new_expense + new_wants + new_savings
+
         if new_total != 100:
             print("Those number's don't add up to 100, please try again.")
             continue
@@ -163,14 +175,58 @@ def change_plan(bp):
             print("Invalid input, please try again.")
             continue
 
-# this function shows the user a recommended budget allocation amount
-def rec_exp(d1, d2):
-    d1_values = d1.values()
-    d1_total = sum(d1_values)
-    d2_values = d2.values()
-    d2_total = sum(d2_values)
-    recommended_ratio = int((d1_total / d2_total) * 100)
-    print("Based on your financial standing, the recommended expense allocation ratio is: " + str(recommended_ratio) + ".")
+
+# this function allows the user to view specific dictionaries
+def view_dict(aDict, name_of):
+    """
+    aDict: any dictionary the user would like to view
+    name_of: just used to display the name of the dictionary being accessed
+    """
+    print("Currently Viewing: " + name_of)
+
+    for key, value in aDict.items():
+        print(key + " : " + str(value))
+
+
+# this function shows the user a recommended income for their current financial situation
+def rec_inc():
+    actual_ratio = int((exp_total / inc_total) * 100)
+    plan_ratio = budget_plan['expenses']
+    needed_inc = int(exp_total / (plan_ratio / 100))
+
+    print("Actual Expense to Income Ratio: " + str(actual_ratio))
+    print("Budget Plan Expence to Income Ratio: " + str(plan_ratio))
+    print("Current income: $" + str(inc_total))
+    print("Needed income: $" + str(needed_inc))
+
+    if actual_ratio == plan_ratio:
+        print("Your actual ratio is equal to your expense plan, this means you do not need to change your income.")
+    elif actual_ratio < plan_ratio:
+        print("Your actual ratio is BELOW your expense plan, this means that you may have enough income to compensate expenses.")
+    else:
+        print("Your actual ratio is ABOVE your expense plan, this means that you would need to increase your income.")
+        print("You would need to make at least: $" + str(needed_inc) + ", to meet your expense plan.")
+
+
+# this function shows the user a recommended expense for their current financial situation
+def rec_exp():
+    actual_ratio = int((exp_total / inc_total) * 100)
+    plan_ratio = budget_plan['expenses']
+    needed_exp = int(inc_total * (plan_ratio / 100))
+
+    print("Actual Expense to Income Ratio: " + str(actual_ratio))
+    print("Budget Plan Expence to Income Ratio: " + str(plan_ratio))
+    print("Current expenses: $" + str(exp_total))
+    print("Needed expense: $" + str(needed_exp))
+
+    if actual_ratio == plan_ratio:
+        print("Your actual ratio is equal to your expense plan, this means you do not need to change your expenses.")
+    elif actual_ratio < plan_ratio:
+        print("Your actual ratio is BELOW your expense plan, this means you have more than enough income to compensate expenses.")
+    else:
+        print("Your actual ratio is ABOVE your expense plan, this means that you would need to decrease your expenses.")
+        print("Your expenses would need to be at most: $" + str(needed_exp) + ", to meet your expense plan.")
+
 
 # this function tells the user what range percentage their 'want' falls under
 def want_this():
@@ -198,6 +254,7 @@ def want_this():
     else:
         print("Invalid input.")
 
+
 # this function allows the user to set a save plan with a target goal and monthly timeframe
 def save_plan():
     save_goal = int(input("How much money would you like to save? "))
@@ -207,6 +264,7 @@ def save_plan():
     print("In order to save $" + str(save_goal) + " in " + str(save_time) + " month(s), you would need to save $" + str(save_per_month))
     print("This means you would need to allocate at least " + str((round(save_plan, 2))) + " for your 'savings' budget.")
     print("Your current savings allocation is: " + str(budget_plan['savings']))
+
 
 # this function allows the user to play with the dictionaries to 
 def budget_play(aDict, type):
@@ -246,25 +304,64 @@ def budget_play(aDict, type):
             print("Invalid input.")
             continue
 
+
+# Setting initial values for the income, expense, and budget plan
+initial_set = True
+while initial_set == True:
+    print("Now we are going to ask for some information regarding your finances.")
+    print("First we are going to enter your income information.")
+    add_to(inc_info)
+
+    print("Now we are going to enter your expense information.")
+    add_to(exp_info)
+
+    while True:
+        print("Finally, we are going to enter your budget plan preference.")
+        print("Note: the default budget plan is: expenses-50, wants-30, savings-20.")
+        budget_set = input("Would you like to keep the default budget plan? y/n: ").lower()
+        if budget_set == 'y':
+            break
+        elif budget_set == 'n':
+            change_plan(budget_plan)
+        else:
+            print("Invalid input.")
+            continue
+
+    print("Income Info: " + str(inc_info))
+    print("Expense Info: " + str(exp_info))
+    print("Budget Plan: " + str(budget_plan))
+
+    full_confirm = input("Are you happy with the current information (you may make edits later)? y/n: ")
+
+    if full_confirm == 'y':
+        initial_set = False   # This is so that this prompt isn't asked again
+        break
+    elif full_confirm == 'n':
+        continue
+    else:
+        print("Invalid input.")
+        continue
+
+
 # Terminal interface that the user will interact with
 while True:
     print("""
     Sections Menu:
-    1. Modify Budget Information
+    1. Modify Information
     2. Budget Analysis
     3. Budget Play
-    4. View Budget
+    4. View Information
     Enter 'v' to view all actions within each section.
     Enter 'q' to quit.
     """)
 
-    choice = input("What section would you like to choose? Enter number: ")
+    choice = str(input("What section would you like to choose? Enter number: ")).lower()
 
-    if choice == 'q' or 'Q':
+    if choice == 'q':
         print("Thank you for using our service. Come again!")
         exit()
 
-    elif choice == 'v' or 'V':
+    elif choice == 'v':
         print("""
         1. Modify Budget Information
             a. Modify income
@@ -275,45 +372,139 @@ while True:
             e. Recommended expense
             f. Wants analysis
         3. Budget Play
-            g. Play with different budget rules
-            h. Play with current expenses
-            i. Play with current income
+            g. Play with current expenses
+            h. Play with current income
         4. View Budget
-            j. View all budget information
-            k. View all expense information
-            l. View all income information
-            m. View all wants information
+            i. View all budget information
+            j. View all expense information
+            k. View all income information
+            l. View all information
         """)
+
 
     elif choice == '1':
         print("""
-        a. Modify income (fixed and variable)
-        b. Modify expenses (fixed and variable)
-        c. Modify savings goal
+        a. Add item
+        b. Delete item
+        c. Modify value
         """)
+
+        sub_choice1 = input("Which subchoice would you like to select (r to return)? ").lower()
         
+        if sub_choice1 == 'a':
+            while True:
+                add_where = input("Would you like to add to income (i) or expenses (e) (q to quit)? ").lower()
+                if add_where == 'q':
+                    print("Returning to main menu.")
+                    break
+                elif add_where == 'i':
+                    add_to(inc_info)
+                elif add_where == 'e':
+                    add_to(exp_info)
+                else:
+                    print("Invalid input.")
+                    continue
+        elif sub_choice1 == 'b':
+            while True:
+                del_where = input("Would you like to delete from income (i) or expenses (e) (q to quit)? ").lower()
+                if del_where == 'q':
+                    print("Returning to main menu.")
+                    break
+                elif del_where == 'i':
+                    del_from(inc_info)
+                elif del_where == 'e':
+                    del_from(exp_info)
+                else:
+                    print("Invalid input.")
+                    continue
+        elif sub_choice1 == 'c':
+            while True:
+                mod_where = input("Where would you like to modify a value, income (i) or expenses (e) (q to quit)? ").lower()
+                if mod_where == 'q':
+                    print("Returning to main menu.")
+                    break
+                elif mod_where == 'i':
+                    mod_amount(inc_info)
+                elif mod_where == 'e':
+                    mod_amount(exp_info)
+                else:
+                    print("Invalid input.")
+                    continue
+        elif sub_choice1 == 'r':
+            continue
+        else:
+            print("Invalid input.")
+            continue
+        
+
     elif choice == '2':
         print("""
         d. Recommended income
         e. Recommended expense
         f. Wants analysis
         """)
+
+        sub_choice2 = input("Which subchoice would you like to select (r to return)? ").lower()
+
+        if sub_choice2 == 'd':
+            rec_inc()
+        elif sub_choice2 == 'e':
+            rec_exp()
+        elif sub_choice2 == 'f':
+            want_this()
+        elif sub_choice2 == 'r':
+            continue
+        else:
+            print("Invalid input.")
+            continue
         
+
     elif choice == '3':
         print("""
-        g. Play with different budget rules
-        h. Play with current expenses
-        i. Play with current income
+        g. Play with current expenses
+        h. Play with current income
         """)
+
+        sub_choice3 = input("Which subchoice would you like to select (r to return)? ").lower()
+
+        if sub_choice3 == 'g':
+            budget_play(exp_info, 'exp')
+        elif sub_choice3 == 'h':
+            budget_play(inc_info, 'inc')
+        elif sub_choice3 == 'r':
+            continue
+        else:
+            print("Invalid input.")
+            continue
         
+
     elif choice == '4':
         print("""
-        j. View all budget information
-        k. View all expense information
-        l. View all income information
-        m. View all wants information
+        i. View all budget information
+        j. View all expense information
+        k. View all income information
+        l. View all information
         """)
-        
+
+        sub_choice4 = input("Which subchoice would you like to select (r to return)? ").lower()
+
+        if sub_choice4 == 'i':
+            view_dict(budget_plan, 'Budget Plan')
+        elif sub_choice4 == 'j':
+            view_dict(exp_info, 'Expenses')
+        elif sub_choice4 == 'k':
+            view_dict(inc_info, 'Income')
+        elif sub_choice4 == 'l':
+            view_dict(budget_plan, 'Budget Plan')
+            view_dict(exp_info, 'Expenses')
+            view_dict(inc_info, 'Income')
+        elif sub_choice4 == 'r':
+            continue
+        else:
+            print("Invalid input.")
+            continue
+
+
     else:
         try_again = input("Invalid input. Would you like to try again? y/n: ")
         if try_again == "y":
@@ -322,6 +513,5 @@ while True:
             print("Thank you for using our service. Come again!")
             exit()
         else:
-            print("Bro.")
-            exit()
-
+            print("Invalid input.")
+            continue
